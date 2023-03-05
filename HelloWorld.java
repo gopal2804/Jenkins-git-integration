7fc5ec0c1bf14d9228bb4079191e942f08197d7e
GOPAL GUPTA <51901743+gopal2804@users.noreply.github.com>
2023-03-05 16:02:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello gfd,nfdbddjhfddgduegfkurfgiuopal");
-		System.out.println("Hello gfd,nfdbddjhfddgduegfkurfgiuopal");
 	}
 }
